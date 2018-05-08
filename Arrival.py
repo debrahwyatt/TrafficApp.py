1646843aefcaae7e650584af996fb4cf3214c7f1
Devlin Wyatt <devlinwyatt@gmail.com>
2018-05-07 18:37:10 -0700

    Update Arrival.py
    Version 1.1, more accurate algorithms and the ability to input arrival dates and times.

--- Arrival.py
+++ Arrival.py
@@ -1,31 +1,69 @@
 #!/usr/bin/env python3
 
 '''
-Arrival.py v1.0
+Arrival.py v1.1
 
-Application is used to determine optimal departure times.
+Application is used to determain optimal departure times.
+
+Needs conversion from day to day.
+Needs error messages when times arnt available.
+Still needs estimate when times arn't available.
+	
 
 '''
 
 
 import re
-import datetime
-from time import sleep
-from urllib.request import urlopen
 
-print("\nRUNNING: test.py\n\n")
+print("\nRUNNING: Arrival_v1.1.py\n\n")
 
 def main():
 		
-	#Program parameters		
-	day = 'Tuesday'
-	arrival_time = time = '17:00'
 	g=[]
 	count = 0
 
+	#Program parameters		
+	while True:
+		day = input("For what day of the week? ")
+		weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+		if day in weekday:
+			break
+	
+	#User input for arrival time
+	arrival_time = time = input("\nWhat time? (use 24 hour format eg. 15:45) ")
+
+
+	'''
+	#Supposed to check valid input from user.
+
+	while True:
+		arrival_time = time = input("\nWhat time? (use 24 hour format eg. 15:45) ")
+		user_test = re.findall('^(\d?\d:\d\d)$', time)
+		print(user_test)
+
+		if None in user_test:
+			print('hah')
+			break
+	'''
+	
+	#user check for travel option
+	while True:
+		user = int(input("\nFrom home to work = 1\nFrom work to home = 2\n"))
+		
+		if user == 1:
+			filename = '3023+freethy+pl_to_uvic.csv'
+			break
+
+		if user == 2:
+			filename = 'uvic_to_3023+freethy+pl.csv'
+			break
+
+
+
 	try:		
 		#Open the file with desired route		
-		file1 = open('3023+freethy+pl_to_uvic.csv', 'r')
+		file1 = open(filename, 'r')
 		file_info = file1.read()
 
 		while True:
@@ -48,20 +86,29 @@ def main():
 			temp_minute = int(m[0][1])
 			temp_hour = int(m[0][0])
 
+			#Rolls time over 
 			if temp_minute == 0:
 				temp_minute = 60
 				temp_hour = temp_hour - 1
 
+			#compensates int to string conversion
 			temp_minute = temp_minute - 5
+			if temp_minute == 5:
+				temp_minute = '05'
+			if temp_minute == 0:
+				temp_minute = '00'
+
+			#Progression counter
 			count = count + 5
 
+			#converts time back into string
 			time = str(temp_hour) + ':' + str(temp_minute)
 
+			#Determins departure time
 			if check <= count:
-				print(time)
-				print(check)
+				print("Time to get there: ", time)
+				print("Travel time: ", check)
 				return
-
 
 	except:
 		print("Somthing went wrong")
